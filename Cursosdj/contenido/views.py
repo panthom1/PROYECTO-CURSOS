a37d9386dc07122fa466188ed76c738b5cf6f0ce
panthom1 <cardan.ba@hotmail.com>
2024-06-30 21:01:34 -0600

    Agregando proyecto cursos

---
+++ Cursosdj/contenido/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+
+
+
+
+# Create your views here.
+def principal(request):
+   
+    return render(request,"contenido/principal.html")
+
+def contacto(request):
+    
+    return render(request,"contenido/contacto.html")
+
+def curso(request):
+
+  
+    return render(request,"contenido/curso.html")
